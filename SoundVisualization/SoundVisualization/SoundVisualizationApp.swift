85a8e045fb0b5aebd77b8e73c9eeaece99569b3b
Dominic(Dongkyu)Lee <ciuizz@naver.com>
2023-07-22 18:11:14 +0900

    initialize project

---
+++ SoundVisualization/SoundVisualization/SoundVisualizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundVisualizationApp.swift
+//  SoundVisualization
+//
+//  Created by 이동규 on 2023/07/22.
+//
+
+import SwiftUI
+
+@main
+struct SoundVisualizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
